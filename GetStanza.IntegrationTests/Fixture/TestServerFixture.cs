12d600182bb94947a769439032e0ca49b2342ec1
Nathan Barlow <nathan.barlow@sre.is>
2024-07-09 13:46:30 -0500

    Add GetStanza.IntegrationTests

---
+++ GetStanza.IntegrationTests/Fixture/TestServerFixture.cs
@@ -0,0 +1,35 @@
+using GetStanza.IntegrationTests.Fakes;
+using Microsoft.AspNetCore.Hosting;
+using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace GetStanza.IntegrationTests.Fixture;
+
+public sealed class TestServerFixture : IDisposable
+{
+    private static readonly int hubPort = 5000;
+    private readonly IHost _host;
+
+    public TestServerFixture()
+    {
+        _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>().ConfigureKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, hubPort,
+                        listenOptions => { listenOptions.Protocols = HttpProtocols.Http2; });
+                });
+            }).Build();
+        _host.RunAsync();
+    }
+
+    public string HubAddress = $"http://localhost:{hubPort}";
+
+    public void Dispose()
+    {
+        _host.StopAsync();
+    }
+}
