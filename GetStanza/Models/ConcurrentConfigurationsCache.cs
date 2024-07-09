eccb430ed5322a1a8667cf34b2510717ba751620
Nathan Barlow <nathan.barlow@sre.is>
2024-07-09 19:06:51 +0000

    Correct incorrect summary in ConcurrentConfigurationsCache

--- GetStanza/Models/ConcurrentConfigurationsCache.cs
+++ GetStanza/Models/ConcurrentConfigurationsCache.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 namespace GetStanza.Models;
 
 ///<summary>
-/// This class serves as the thread safe storage of guard and service configurations.
+/// This class serves as the thread safe storage of guard configurations.
 /// Only one instance of this class should ever exist for any one StanzaClient instance.
 ///<\summary>
 public class ConcurrentConfigurationsCache()
