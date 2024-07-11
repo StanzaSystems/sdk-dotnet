301e9d3b5334faa18a9492d567288171a14b62f6
James Orson <jamesaorson@gmail.com>
2024-07-11 18:42:19 +0000

    Unnest namespace

--- GetStanza.IntegrationTests.Fakes/Program.cs
+++ GetStanza.IntegrationTests.Fakes/Program.cs
@@ -5,34 +5,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
-namespace GetStanza.IntegrationTests.Fakes
-{
-    internal static class Program
-    {
-        private static async Task Main(string[] args)
-        {
-            await CreateHostBuilder(args).Build().RunAsync();
-        }
+namespace GetStanza.IntegrationTests.Fakes;
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                        .UseStartup<Startup>()
-                        .ConfigureKestrel(options =>
-                        {
-                            options.Listen(
-                                IPAddress.Any,
-                                5000,
-                                listenOptions =>
-                                {
-                                    listenOptions.Protocols = HttpProtocols.Http2;
-                                }
-                            );
-                        });
-                });
-        }
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await CreateHostBuilder(args).Build().RunAsync();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder
+                    .UseStartup<Startup>()
+                    .ConfigureKestrel(options =>
+                    {
+                        options.Listen(
+                            IPAddress.Any,
+                            5000,
+                            listenOptions =>
+                            {
+                                listenOptions.Protocols = HttpProtocols.Http2;
+                            }
+                        );
+                    });
+            });
     }
 }
